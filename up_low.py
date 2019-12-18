77ce61a19b1868b74548d75252e334deb9d2004e
Bogdan Ganea <bogdanionutganea@gmail.com>
2019-12-18 09:31:46 +0200

    Update up_low.py
    Added docstring to explain the function

--- up_low.py
+++ up_low.py
@@ -1,11 +1,12 @@
-# Write a Python function that accepts a string and calculates the number of upper case letters and lower case letters.
-# 
-# Sample String : 'Hello Mr. Rogers, how are you this fine Tuesday?'
-# Expected Output : 
-# No. of Upper case characters : 4
-# No. of Lower case Characters : 33
-
 def up_low(s):
+    '''
+    Write a Python function that accepts a string and calculates the number of upper case letters and lower case letters.
+     
+    Sample String : 'Hello Mr. Rogers, how are you this fine Tuesday?'
+    # Expected Output : 
+    # No. of Upper case characters : 4
+    # No. of Lower case Characters : 33
+    '''
     ups = 0
     lows = 0
     for letter in s:
